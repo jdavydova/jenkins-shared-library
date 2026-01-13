60c2d964997fa261e84ed2e2b3170ef286b4480b
Julia Davydova <juliada888@gmail.com>
2026-01-13 10:51:57 +0200

    updated docker login

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -14,9 +14,13 @@ class Docker implements Serializable {
         script.sh "docker build -t $imageName ."
     }
 
-    def dockerLogin() {
-        script.withCredentials([script.usernamePassword(credentialsId: 'docker-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-            script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
+    def dockerLogin(String credentialsId) {
+        script.withCredentials([script.usernamePassword(
+                credentialsId: credentialsId,
+                usernameVariable: 'USER',
+                passwordVariable: 'PASS'
+        )]) {
+            script.sh 'echo "$PASS" | docker login -u "$USER" --password-stdin'
         }
     }
 
